5113ba1e460325db62b37530ec583f6f5f4ecbc4
Giancarlo Frix <gfrix@rocketsoftware.com>
2018-09-10 19:19:00 -0400

    define a hurd-style open() wrapper
    We can't directly use the linux implementation without a level of
    indirection since we don't have an openat syscall.

---
+++ sysdeps/unix/sysv/zos/open.c
@@ -0,0 +1,58 @@
+/* z/OS open syscall implementation.
+   Copyright (C) 2018 Rocket Software.
+   This file is part of the GNU C Library.
+   Contributed by Giancarlo Frix <gfrix@rocketsoftware.com>
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.
+
+   We don't use the Linux implementation because there's no openat
+   equivalent BPX syscall, and we don't need to worry about
+   cancellation. */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdarg.h>
+
+#include <sysdep.h>
+
+/* Open FILE with access OFLAG.  If O_CREAT or O_TMPFILE is in OFLAG,
+   a third argument is the file protection.  */
+int
+__libc_open (const char *file, int oflag, ...)
+{
+  int mode = 0;
+
+  if (__OPEN_NEEDS_MODE (oflag))
+    {
+      va_list arg;
+      va_start (arg, oflag);
+      mode = va_arg (arg, int);
+      va_end (arg);
+    }
+
+  return INLINE_SYSCALL_CALL (open, file, oflag, mode);
+}
+libc_hidden_def (__libc_open)
+
+weak_alias (__libc_open, __open)
+libc_hidden_weak (__open)
+weak_alias (__libc_open, open)
+
+/* open64 is just the same as open for us.  */
+weak_alias (__libc_open, __libc_open64)
+weak_alias (__libc_open, __open64)
+libc_hidden_weak (__open64)
+weak_alias (__libc_open, open64)
---
+++ sysdeps/unix/sysv/zos/open64.c
@@ -0,0 +1,2 @@
+/* open64 is defined in open.c as an alias.
+   This file is just here to satisfy the build system. */
