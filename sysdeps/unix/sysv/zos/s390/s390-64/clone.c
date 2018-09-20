b9a52bb423bfda823d113bbf17785a4b3b8255ba
Giancarlo Frix <gfrix@rocketsoftware.com>
2018-09-20 12:06:34 -0400

    Dummy out clone

---
+++ sysdeps/unix/sysv/zos/s390/s390-64/clone.c
@@ -0,0 +1,33 @@
+/* Copyright (C) 2018 Rocket Software
+   This file is part of the GNU C Library.
+   Contributed by Giancarlo Frix <gfrix@rocketsoftware.com>, 2018.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.
+
+   TODO: This file is a dummy right now.  */
+
+#include <errno.h>
+
+int __clone (int (*fn)(void *), void *child_stack, int flags,
+	   void *arg, ...)
+{
+  __set_errno (ENOSYS);
+  return -1;
+}
+
+libc_hidden_def (__clone)
+stub_warning (__clone)
+weak_alias (__clone, clone)
+
