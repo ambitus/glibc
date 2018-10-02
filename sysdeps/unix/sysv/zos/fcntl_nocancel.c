c83611e6d85ef6c3ddbc1db94888e99e0857881f
Giancarlo Frix <gfrix@rocketsoftware.com>
2018-10-01 20:42:48 -0400

    Override the linux fcntl_nocancel.c
    The linux version uses constants that we don't support right now.

---
+++ sysdeps/unix/sysv/zos/fcntl_nocancel.c
@@ -0,0 +1,47 @@
+/* z/OS fcntl syscall implementation -- non-cancellable.
+   Copyright (C) 2018 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+
+#include <fcntl.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <sysdep-cancel.h>
+#include <not-cancel.h>
+
+#ifndef __NR_fcntl64
+# define __NR_fcntl64 __NR_fcntl
+#endif
+
+int
+__fcntl64_nocancel (int fd, int cmd, ...)
+{
+  va_list ap;
+  void *arg;
+
+  va_start (ap, cmd);
+  arg = va_arg (ap, void *);
+  va_end (ap);
+
+  return __fcntl64_nocancel_adjusted (fd, cmd, arg);
+}
+hidden_def (__fcntl64_nocancel)
+
+int
+__fcntl64_nocancel_adjusted (int fd, int cmd, void *arg)
+{
+  return INLINE_SYSCALL_CALL (fcntl64, fd, cmd, (void *) arg);
+}
