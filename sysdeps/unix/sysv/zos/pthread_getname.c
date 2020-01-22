31a1bc919b7a98510d308ee590c421510184d2dd
Giancarlo Frix <gfrix@rocketsoftware.com>
2020-01-22 15:43:45 -0500

    Use stub implementations of some libpthread syms
    Specifically pthread_getname_np and pthread_setname_np.
    
    INCU-181

---
+++ sysdeps/unix/sysv/zos/pthread_getname.c
@@ -0,0 +1,2 @@
+/* z/OS TODO: Maybe implement this using pthread_tag_np.  */
+#include <nptl/pthread_getname.c>
---
+++ sysdeps/unix/sysv/zos/pthread_setname.c
@@ -0,0 +1,2 @@
+/* z/OS TODO: Maybe implement this using pthread_tag_np.  */
+#include <nptl/pthread_setname.c>
