50a156113169e099d8ea082a58208011e089b4d5
Giancarlo Frix <gfrix@rocketsoftware.com>
2020-05-22 09:08:53 -0400

    Dummy out __pthread_initialize_pids for now
    INCU-226

---
+++ sysdeps/unix/sysv/zos/pthread-pids.h
@@ -0,0 +1,31 @@
+/* Initialize pid and tid fields of struct pthread.  Linux version.
+   Copyright (C) 2020 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+
+#include <pthreadP.h>
+#include <sysdep.h>
+
+/* Initialize PD->pid and PD->tid for the initial thread.  If there is
+   setup required to arrange that __exit_thread causes PD->tid to be
+   cleared and futex-woken, then this function should do that as well.  */
+/* z/OS TODO: IMPORTANT: THREADING: Implement this. This is also where
+   emergency futex waking preparation should occur.  */
+static inline void
+__pthread_initialize_pids (struct pthread *pd)
+{
+  pd->tid = -1;
+}
