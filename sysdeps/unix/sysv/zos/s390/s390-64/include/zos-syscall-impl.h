ambitus/glibc

commit 02ea187260031184ad4c3c185330f3bf01f007d9

Add support of ascii scripts in execve() syscall

sysdeps/unix/sysv/zos/s390/s390-64/include/zos-syscall-impl.h
@@ -2004,7 +2004,9 @@ __zos_sys_execve (int *errcode, const char *pathname, char *const argv[],
   char *translated;
   void *lens;
 
-  extern int printf(const char *, ...);
+  uint32_t def_envlen = 17;
+  const char def_env[] = "_BPXK_AUTOCVT=ON";
+
   /* Support argv == NULL or envp == NULL like linux does.  */
   char *const dummy[] = { NULL };
   if (argv == NULL)
@@ -2026,6 +2028,7 @@ __zos_sys_execve (int *errcode, const char *pathname, char *const argv[],
   count_args (argv, argc);
   count_args (envp, envc);
 #undef count_args
+  envc++;
 
   /* Structure of this allocation:
      -----------------------
@@ -2068,6 +2071,7 @@ __zos_sys_execve (int *errcode, const char *pathname, char *const argv[],
   envlens = (uint32_t *) ((uintptr_t) lens + envlen_off);
 
   total_size = 0;
+  envc--;
 #define count_lengths(list, count, lengths, len_ptrs)			\
   do {									\
     for (uint32_t i = 0; i < count; i++)				\
@@ -2088,6 +2092,7 @@ __zos_sys_execve (int *errcode, const char *pathname, char *const argv[],
   count_lengths (argv, argc, arglens, arglen_ptrs);
   count_lengths (envp, envc, envlens, envlen_ptrs);
 #undef count_lengths
+  envlen_ptrs[envc] = &def_envlen;
 
   translated = __storage_obtain_simple ((total_size + 7UL) & ~7UL);
   if (translated == NULL)
@@ -2111,10 +2116,15 @@ __zos_sys_execve (int *errcode, const char *pathname, char *const argv[],
   copy_and_translate (argv, argc, arglens, args);
   copy_and_translate (envp, envc, envlens, envs);
 #undef copy_and_translate
+  envs[envc] = translated;
+  tr_e_until_len (def_env, translated, def_envlen);
+  envc++;
 
   /* z/OS TODO: We might want to define an exit to clean up any
      resources.  */
   void *exit_addr = NULL, *exit_params = NULL;
+
+  set_prog_ccsid (1047);
 
   BPX_CALL (exec, __bpx4exc_t, &path_len, translated_path, &argc,
 	    arglen_ptrs, args, &envc, envlen_ptrs, envs, &exit_addr,

commit 3e75fbb77c2dfce9d63676353df01224cbcb5696

Fix execve() env size calculation
Fix the crash caused by incorrect size caluclation introduced in
02ea187260031.

INCU-260

sysdeps/unix/sysv/zos/s390/s390-64/include/zos-syscall-impl.h
@@ -2017,10 +2017,6 @@ __zos_sys_execve (int *errcode, const char *pathname, char *const argv[],
   char *translated;
   void *lens;
 
-  /* Part of a workaround to allow us to exec ascii scripts.  */
-  uint32_t def_envlen = 17;
-  const char def_env[] = "_BPXK_AUTOCVT=ON";
-
   /* Support argv == NULL or envp == NULL like linux does.  */
   char *const dummy[] = { NULL };
   if (argv == NULL)
@@ -2042,7 +2038,6 @@ __zos_sys_execve (int *errcode, const char *pathname, char *const argv[],
   count_args (argv, argc);
   count_args (envp, envc);
 #undef count_args
-  envc++;
 
   /* Structure of this allocation:
      -----------------------
@@ -2085,7 +2080,6 @@ __zos_sys_execve (int *errcode, const char *pathname, char *const argv[],
   envlens = (uint32_t *) ((uintptr_t) lens + envlen_off);
 
   total_size = 0;
-  envc--;
 #define count_lengths(list, count, lengths, len_ptrs)			\
   do {									\
     for (uint32_t i = 0; i < count; i++)				\
@@ -2106,7 +2100,6 @@ __zos_sys_execve (int *errcode, const char *pathname, char *const argv[],
   count_lengths (argv, argc, arglens, arglen_ptrs);
   count_lengths (envp, envc, envlens, envlen_ptrs);
 #undef count_lengths
-  envlen_ptrs[envc] = &def_envlen;
 
   translated = __storage_obtain_simple ((total_size + 7UL) & ~7UL);
   if (translated == NULL)
@@ -2130,9 +2123,6 @@ __zos_sys_execve (int *errcode, const char *pathname, char *const argv[],
   copy_and_translate (argv, argc, arglens, args);
   copy_and_translate (envp, envc, envlens, envs);
 #undef copy_and_translate
-  envs[envc] = translated;
-  tr_e_until_len (def_env, translated, def_envlen);
-  envc++;
 
   /* z/OS TODO: We might want to define an exit to clean up any
      resources.  */